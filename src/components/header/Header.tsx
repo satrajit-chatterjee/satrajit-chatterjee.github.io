b53d0a69276fab4453e8a91010a1ed1a12d58f57
Satrajit Chatterjee <satrajitchatterjee@Satrajits-MacBook-Pro.local>
2022-03-06 12:28:48 -0500

    fixing build issue

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -14,16 +14,16 @@ export default function Header() {
         <Link to="/resume">Resume</Link>
       </div>
       <div className={"icon-links"}>
-        <a href={ResumeFile} download="Aditya Kumar Resume.pdf">
+        <a href={ResumeFile} download="Satrajit Chatterjee Resume.pdf">
           <i className={"fas fa-file-pdf fa-lg"}></i>
         </a>
-        <a href="https://www.linkedin.com/in/adityaa30" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/satrajit-chatterjee" target="_blank" rel="noopener noreferrer">
           <i className={"fab fa-linkedin fa-lg"}></i>
         </a>
-        <a href="https://github.com/adityaa30" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/satrajit-chatterjee" target="_blank" rel="noopener noreferrer">
           <i className={"fab fa-github fa-lg"}></i>
         </a>
-        <a href="mailto:k.aditya00@gmail.com" target="_blank" rel="noopener noreferrer">
+        <a href="mailto:satrajitdchatterjee@gmail.com" target="_blank" rel="noopener noreferrer">
           <i className={"fas fa-envelope fa-lg"}></i>
         </a>
       </div>
